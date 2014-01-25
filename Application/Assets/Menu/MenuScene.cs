ad986cbed9ba357d0c9b73779100b75558af458a
Lucien Zuercher(PM) <l.zuercher@tincan.ch>
2014-01-25 06:51:19 +0200

    Update on the Menu 0.1

--- Application/Assets/Menu/MenuScene.cs
+++ Application/Assets/Menu/MenuScene.cs
@@ -5,7 +5,7 @@ public class MenuScene : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
@@ -17,15 +17,15 @@ public class MenuScene : MonoBehaviour {
 	void OnGUI()
 	{
 		// Make a background box
-		GUI.Box(new Rect(10,10,100,90), "Loader Menu");
+		GUI.Box(new Rect(10,10,100,90), "Game Menu");
 		
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(20,40,80,20), "Level 1")) {
-			Application.LoadLevel(1);
+		if(GUI.Button(new Rect(20,40,80,20), "Start Game")) {
+			Application.LoadLevel("gamescene");
 		}
-		
+
 		// Make the second button.
-		if(GUI.Button(new Rect(20,70,80,20), "Level 2")) {
+		if(GUI.Button(new Rect(20,70,80,20), "Quit Game")) {
 			Application.LoadLevel(2);
 		}
 	}
