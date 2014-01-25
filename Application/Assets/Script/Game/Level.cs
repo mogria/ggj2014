b00a0bee8253c6ba3677caf3612a451f53708d82
Lucien Zuercher(PM) <l.zuercher@tincan.ch>
2014-01-25 09:03:40 +0200

    error repaired

--- Application/Assets/Script/Game/Level.cs
+++ Application/Assets/Script/Game/Level.cs
@@ -6,28 +6,34 @@ public class Level : MonoBehaviour {
 
 	Queue<GameObject> Voxels = new Queue<GameObject>(); 
 
+	float leftBorder;
+	float rightBorder;
+
 	// Use this for initialization
 	void Start () {
 		// Loading Level
-
-		// 
-		var dist = (transform.position - Camera.main.transform.position).z;
-		var leftBorder = Camera.main.ViewportToWorldPoint(Vector3(0,0,dist)).x;
-		var rightBorder = Camera.main.ViewportToWorldPoint(Vector3(1,0,dist)).x;
 		
-		transform.position.x = Mathf.Clamp(transform.position.x,leftBorder,rightBorder);
+		// 
+		UpdateBorder ();
 
-		while(Voxels.Peek().transform.position.x)
-		AddVoxelToStart (GameObject.Find ("Land1"));
+
+		GameObject land = GameObject.Find ("Land1");
+		//land.transform.position.x = rightBorder;
+
+		//transform.position.x = Mathf.Clamp(transform.position.x,leftBorder,rightBorder);
+
+		//while(Voxels.Peek().transform.position.x)
+			AddVoxelToStart (land);
 	}
 
 	// Creates and adds a voxel in front of the level
 	// and moves the other prefabs by one
 	void AddVoxelToStart(GameObject ground)
 	{
-		/*Voxels.Enqueue(*/Instantiate(ground);//);
+		Voxels.Enqueue((GameObject)Instantiate(ground));
 	}
 
+	// Removes last Voxel
 	void RemoveLastVoxel()
 	{
 		if (Voxels.Count > 0)
@@ -36,6 +42,16 @@ public class Level : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-	
+		
+	}
+
+	// Updates border, so there is no 
+	void UpdateBorder() {
+		float dist = (transform.position - Camera.main.transform.position).z;
+
+		//leftBorder = Camera.main.ViewportToWorldPoint(Vector3(0,0,dist)).x;
+		//rightBorder = Camera.main.ViewportToWorldPoint(Vector3(1,0,dist)).x;
+
+
 	}
 }
